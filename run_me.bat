deb2e7e79ebb7c283a52ffe027d176cc5cd8c990
Toni Jung <tjung@mail.usf.edu>
2014-07-18 02:01:43 -0400

    Updated Python path

--- run_me.bat
+++ run_me.bat
@@ -1,6 +1,6 @@
 ::Be sure to updated the directories.
 
 "C:\Program Files (x86)\Stata12\StataSE-64.exe" /e do C:\SMS\test.do
-C:\Python27\ArcGISx6410.1\python.exe C:\SMS\test.py
+C:\Python27\python.exe C:\SMS\test.py
 
 PAUSE
